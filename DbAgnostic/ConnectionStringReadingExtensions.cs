acacd6ac8531ece423ae772774c86ddbf1e90c3a
Roger Kratz <roger.kratz@gmail.com>
2023-08-24 12:42:05 +0200

    clean up

--- DbAgnostic/ConnectionStringReadingExtensions.cs
+++ DbAgnostic/ConnectionStringReadingExtensions.cs
@@ -1,59 +1,58 @@
 using System.Data.SqlClient;
 using Npgsql;
 
-namespace DbAgnostic
+namespace DbAgnostic;
+
+public static class ConnectionStringReadingExtensions
 {
-	public static class ConnectionStringReadingExtensions
+	public static string DatabaseName(this string connectionString)
 	{
-		public static string DatabaseName(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).InitialCatalog,
-				() => new NpgsqlConnectionStringBuilder(connectionString).Database);
-		}
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).InitialCatalog,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Database);
+	}
 
-		public static string ServerName(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).DataSource,
-				() => new NpgsqlConnectionStringBuilder(connectionString).Host);
-		}
+	public static string ServerName(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).DataSource,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Host);
+	}
 
-		public static string ApplicationName(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).ApplicationName,
-				() => new NpgsqlConnectionStringBuilder(connectionString).ApplicationName);
-		}
+	public static string ApplicationName(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).ApplicationName,
+			() => new NpgsqlConnectionStringBuilder(connectionString).ApplicationName);
+	}
 
-		public static string Password(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).Password,
-				() => new NpgsqlConnectionStringBuilder(connectionString).Password
-			);
-		}
+	public static string Password(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).Password,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Password
+		);
+	}
 
-		public static string UserName(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).UserID,
-				() => new NpgsqlConnectionStringBuilder(connectionString).Username
-			);
-		}
+	public static string UserName(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).UserID,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Username
+		);
+	}
 
-		public static bool IntegratedSecurity(this string connectionString)
-		{
-			return new ConnectionStringDbSelector(connectionString).PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).IntegratedSecurity,
-				() => new NpgsqlConnectionStringBuilder(connectionString).IntegratedSecurity);
-		}
+	public static bool IntegratedSecurity(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).IntegratedSecurity,
+			() => new NpgsqlConnectionStringBuilder(connectionString).IntegratedSecurity);
+	}
 
-		public static int ConnectionTimeout(this string connectionString)
-		{
-			return connectionString.PickFunc(
-				() => new SqlConnectionStringBuilder(connectionString).ConnectTimeout,
-				() => new NpgsqlConnectionStringBuilder(connectionString).Timeout);
-		}
+	public static int ConnectionTimeout(this string connectionString)
+	{
+		return connectionString.PickFunc(
+			() => new SqlConnectionStringBuilder(connectionString).ConnectTimeout,
+			() => new NpgsqlConnectionStringBuilder(connectionString).Timeout);
 	}
 }
